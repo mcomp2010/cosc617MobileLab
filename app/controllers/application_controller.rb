ea144da341b89323d8ff5d50e5c634a0fa8922bc
Manny <mcomp2010@yahoo.com>
2013-12-03 21:09:04 -0500

    added mobile stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #protect_from_forgery
+  respond_to_mobile_requests :skip_xhr_requests => false
+
+
 end
